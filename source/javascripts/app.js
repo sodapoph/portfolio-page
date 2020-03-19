4e945dd264b3ffec41ced887d7c682d39e1e9ff4
Soraida Henne <sodapoph@gmail.com>
2020-03-19 15:51:54 +0900

    adding favicons to contact

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,3 +1,9 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+// $(".menu-toggle").on('click', function() {
+//   $(this).toggleClass("on");
+//   $('.menu-section').toggleClass("on");
+//   $("nav ul").toggleClass('hidden');
+// });
